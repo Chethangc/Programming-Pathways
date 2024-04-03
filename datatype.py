d7844698ca2b235f7909d9f8364ec6786cf10ddf
chethangc0001@gmail.com <chethangc0001@gmail.com>
2024-04-03 22:41:02 +0900

    [Python] Add datatypes.py. Remove the varaibles.py. Adding all the datatypes of python.

---
+++ datatype.py
@@ -0,0 +1,23 @@
+# TODO: Complete the code
+# TODO: Test the code
+# TODO: Analyze the code and add additional code for better explaination of datatype.
+
+# Numeric Datatype
+intnumber = 1  # Integer
+floatnumber = 1.0  # Floting
+complexnumber = 1  # Complex
+
+# Bool Type
+booldata = True  # Boolean (True|False)
+
+# Sequence Type
+string = "This is the string"  # String
+listdata = []   # List
+tupledata = ()  # Tuple
+
+# Mapping Datatype
+dictdatatype = {}  # Dict
+
+# Set Datatype
+setdata = {}   # Set
+frozensetdata = {}  # Frozenset
