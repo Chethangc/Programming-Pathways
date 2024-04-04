40f2b20d8a37b4b9d5ed5aa8bebac08a1d4e58fc
chethangc0001@gmail.com <chethangc0001@gmail.com>
2024-04-04 20:36:11 +0900

    [Python] updating datatypes. Numerical and String

--- datatype.py
+++ datatype.py
@@ -2,16 +2,26 @@
 # TODO: Test the code
 # TODO: Analyze the code and add additional code for better explaination of datatype.
 
+# No Decleration required
 # Numeric Datatype
-intnumber = 1  # Integer
-floatnumber = 1.0  # Floting
-complexnumber = 1  # Complex
+intnumber = 1  # Integer (no limit)
+#longnumber = 10**1000 # Long depricated in 3.x
+floatnumber = 1.0345  # Floting accurate upto 15 decimal places
+complexnumber = 100+3j  # Complex Number
 
-# Bool Type
-booldata = True  # Boolean (True|False)
+print("Type of intnumber ", type(intnumber))
+print("Type of floatnumber ", type(floatnumber))
+print("Type of complexnumber ", type(complexnumber))
 
-# Sequence Type
-string = "This is the string"  # String
+
+# Bool Datatype
+booldata = True  # Boolean (True or False)
+
+# Sequence Datatype
+# String Type
+string = "This is the string"  # Double quoted string
+singlequotestring = 'This is string' # Single quoted string
+
 listdata = []   # List
 tupledata = ()  # Tuple
 
